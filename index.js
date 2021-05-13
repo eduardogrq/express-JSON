f7079247754a54f935b08b2af499916ede763b8c
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-05-12 22:03:19 -0500

    Se crea CRUD y métodos para optimizar código

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
-const { response } = require('express');
+const { response, json } = require('express');
 const express = require('express')
 const server = express()
 
-const fs = require('fs')
+const fs = require('fs');
+const { request } = require('http');
+const { stringify } = require('querystring');
 
 //Middleware
 server.use(express.json())
+
+function getKodersFile(){
+    const content = fs.readFileSync('koders.json', 'utf8')
+    return(JSON.parse(content))
+}
 
 //creamos la ruta de koders
 server.get('/koders', (request, response) => {
@@ -22,6 +29,104 @@ server.get('/koders', (request, response) => {
         })
 })
 
+// Agregaremos un koder a nuestro arreglo de koders
+
+server.post('/koders', (request, response) => {
+    const name = request.body.name
+    const gender = request.body.gender
+
+    const newKoder = {name, gender}
+
+    const content = fs.readFileSync('koders.json', 'utf8')
+    const json = JSON.parse(content)
+
+    json.koders.push(newKoder)
+
+    fs.writeFileSync('koders.json', JSON.stringify(json, null, 2), 'utf8')
+
+    response.json({
+        success: true
+    })
+
+})
+
+// Actualizamos un koder de nuestro arreglo
+server.patch('/koders/:id', (request, response) => {
+    const id = parseInt(request.params.id) //string
+    const name = request.body.name
+
+    const kodersJSON = getKodersFile()
+
+    const newKoders = kodersJSON.koders.reduce((koders, koderActual) => {
+        if(id === koderActual.id){
+            koderActual.name = name
+        }
+
+        return[
+            ...koders,
+            koderActual
+        ]
+    }, [])
+
+    kodersJSON.koders = newKoders
+
+    fs.writeFileSync('koders.json', JSON.stringify(kodersJSON, null, 2), 'utf8')
+
+    response.json({
+        success: true
+    })
+})
+
+// Practica
+/* 
+Crear un endpoint para borrar
+DELETE /koders/:id
+GET /koders/:id
+*/
+
+//Obteniendo el koder
+server.get('/koders/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+
+    const kodersJSON = getKodersFile()
+
+    let newKoder = kodersJSON.koders.find(koder => koder.id === id)
+
+    if(!newKoder){
+        response.json({
+            // response.status(404),
+            success: false,
+            message: 'Koder not found'
+        })
+    }
+
+    response.json({
+        success: true,
+        message: 'koder Found',
+        data:{
+            koder: newKoder
+        }
+    })
+})
+
+// Eliminar koders
+
+server.delete('/koders/:id', (request, response) => {
+    const id = parseInt(request.params.id)
+
+    const kodersJSON = getKodersFile()
+
+    const newKoders = kodersJSON.koders.filter(koder => koder.id != id)
+    kodersJSON.koders = newKoders
+
+    fs.writeFileSync('koders.json', JSON.stringify(kodersJSON, null, 2), 'utf8')
+
+    response.json({
+        success: true
+    })
+})
+
+// Servidor escuchando
 server.listen(8080, () => {
     console.log('Servidor escuchando en el puerto 8080')
 })
